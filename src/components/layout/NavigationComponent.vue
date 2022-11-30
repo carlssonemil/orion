559ae20d38e0b37c0d5cc8081a08ce4ca7d84da7
Ben A <ben@sol3.me>
2022-11-30 17:44:24 +0000

    Change title text for bug report link

--- src/components/layout/NavigationComponent.vue
+++ src/components/layout/NavigationComponent.vue
@@ -15,7 +15,7 @@
 				<IconComponent name="cog" />
 			</router-link>
 			<a
-				href="https://github.com/carlssonemil/orion/issues/new?title=Bug%20Report%20-%20"
+				href="https://github.com/carlssonemil/orion/issues/new?title=[Bug]%20"
 				target="_blank"
 				content="Report An Issue"
 				v-tippy="{ placement: 'bottom' }">
