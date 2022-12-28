722fa46ccd34d3dd4b95da6fa1ee6c318ed1f3c7
Timon Kobusch <timon_kobusch@web.de>
2022-12-28 23:05:24 +0100

    German Translation

--- src/i18n/locales.js
+++ src/i18n/locales.js
@@ -1,9 +1,11 @@
 import enUS from './locales/en-US.json'
 import svSE from './locales/sv-SE.json'
 import esCO from './locales/es-CO.json'
+import deDE from './locales/de-DE.json'
 
 export default {
 	'en-US': enUS,
 	'sv-SE': svSE,
 	'es-CO': esCO,
+	'de-DE': deDE,
 }
