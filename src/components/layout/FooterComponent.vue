9808b8d6f6dbef524f44cd0a26adcecee21c11a9
Emil Carlsson <hello@emilcarlsson.se>
2022-12-28 16:32:40 +0100

    Add 'translated by' text to fix #69

--- src/components/layout/FooterComponent.vue
+++ src/components/layout/FooterComponent.vue
@@ -4,6 +4,9 @@
 			<i18n path="general.made_by">
 				<a href="https://emilcarlsson.se/">Emil Carlsson</a>
 			</i18n>
+			<i18n v-if="translatedBy" path="general.translated_by">
+				<a :href="translatedBy.url">{{ translatedBy.username }}</a>
+			</i18n>
 		</div>
 
 		<div>
@@ -16,11 +19,26 @@
 </template>
 
 <script>
+import { translatedBy } from '@/i18n/locales.js'
 import LocaleSwitcherComponent from '@/components/LocaleSwitcherComponent.vue'
 
 export default {
 	components: {
 		LocaleSwitcherComponent,
+	},
+
+	computed: {
+		translatedBy() {
+			const currentLocale = this.$root.$i18n.locale
+			const translator = translatedBy[currentLocale]
+
+			if (!translator) return null
+
+			return {
+				url: translator,
+				username: translator.split('/').pop(),
+			}
+		},
 	},
 }
 </script>
--- src/i18n/locales.js
+++ src/i18n/locales.js
@@ -2,6 +2,10 @@ import enUs from './locales/en-US.json'
 import svSE from './locales/sv-SE.json'
 import esCO from './locales/es-CO.json'
 
+export const translatedBy = {
+	'es-CO': 'https://github.com/Whoandozco',
+}
+
 export default {
 	'en-US': enUs,
 	'sv-SE': svSE,
